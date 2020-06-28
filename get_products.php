066c4ef072412719f9168d61d1b20ea2cc26ad1f
tdawson-42 <tdawson@student.hive.fi>
2020-06-28 22:52:50 +0300

    push

--- get_products.php
+++ get_products.php
@@ -1,9 +1,14 @@
 <?php
 function get_products($id) {
 	require('db_management/connect.php');
-	$sql = "SELECT * FROM PRODUCTS WHERE id=$id";
-	if (!$result = mysqli_query($conn, $sql))
-		die("Error: " . $sql . "<br>" . mysqli_error($conn));
+
+	$stmt = mysqli_stmt_init($conn);
+	if (mysqli_stmt_prepare($stmt, "SELECT * FROM PRODUCTS WHERE id=?")) {
+		mysqli_stmt_bind_param($stmt, "i", $id);
+		mysqli_stmt_execute($stmt);
+	}
+	$result = mysqli_stmt_get_result($stmt);
+	mysqli_stmt_close($stmt);
 	return mysqli_fetch_array($result);
 }
 ?>
