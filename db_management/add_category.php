188170d3e2ab57f991ecdae7ce1dab900f3c01b9
Antti Vornanen <avornane@c1r1p11.hive.fi>
2020-06-28 22:56:08 +0300

    Add prepare for new category

--- db_management/add_category.php
+++ db_management/add_category.php
@@ -12,12 +12,12 @@ if(mysqli_num_rows($result) > 0) {
 	die;
 }
 
-$sql = "INSERT INTO CATEGORIES (name) VALUES ('$name')";
-if (mysqli_query($conn, $sql)) {
+$stmt = mysqli_stmt_init($conn);
+if (mysqli_stmt_prepare($stmt, "INSERT INTO CATEGORIES (name) VALUES (?)")) {
+	mysqli_stmt_bind_param($stmt, "s", $name);
+	mysqli_stmt_execute($stmt);
 	echo nl2br("New category created successfully\n");
-} else {
-	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-mysqli_close($conn);
+mysqli_stmt_close($stmt);
 $_SESSION['msg'] = "Category created successfully!";
 header("Location:../manage_categories.php");
